897190150ff0d7fe14abbd05913d0c4ceae8b2c6
106316155@gms.tcu.edu.tw <106316155@gms.tcu.edu.tw>
2021-07-19 17:11:31 +0800

    210719 Update finding form

--- MammoMassObservation_SimpleExample_1/MammoMassObservation_Example/jsonObservation copy.js
+++ MammoMassObservation_SimpleExample_1/MammoMassObservation_Example/jsonObservation copy.js
@@ -1,18 +1,18 @@
 var Observation = {
     "resourceType": "Observation",
     "subject": {
-        "reference": "Patient/2565 "
+        "reference": "Patient/2565"
     },
     "component" : [{ //可放觀察結果
         "code" : { 
             "coding" : [{ 
-                "system" : "MammoMass/", //定義結果代碼系統的url
-                "code" : "DistancenMassCodeSystem" // 存結果代碼
+                "system" : "MammoMass/", // 定義結果代碼系統的url
+                "code" : "DistanceMassCodeSystem"  // 產生結果代碼的系統代碼
              }]},
         "valueCodeableConcept": {
             "coding": [{
-                    "system" : "https://jeshika106316155.github.io/newCodeSystem/DistanceCS.html",
-                    "code" : ""
+                    "system" : "https://jeshika106316155.github.io/newCodeSystem/DistanceCS.html",// 產生結果代碼系統的url
+                    "code" : "" // 存結果代碼
                 }
             ]
         }
@@ -33,5 +33,5 @@ var Observation = {
     }]
 }
 var fhir = {
-    "url": "http://203.64.84.213:8080/fhir/"//"https://hapi.fhir.org/baseR4/"
+    "url": "https://203.64.84.213:8080/fhir/" //"https://hapi.fhir.org/baseR4/"
 }

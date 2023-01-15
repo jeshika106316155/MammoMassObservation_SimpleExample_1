4b216282a302767e6de6920fd000acad39a40687
Jeshika_jesh <62123449+jeshika106316155@users.noreply.github.com>
2023-01-15 23:52:56 +0700

    change FHIR server

--- MammoMassObservation_SimpleExample_1/MammoMassObservation_Example/jsonObservation copy.js
+++ MammoMassObservation_SimpleExample_1/MammoMassObservation_Example/jsonObservation copy.js
@@ -33,5 +33,5 @@ var Observation = {
     }]
 }
 var fhir = {
-    "url": "http://203.64.84.213:8080/fhir/" //"https://hapi.fhir.org/baseR4/"
+    "url": "https://hapi.fhir.org/baseR4/" //"http://203.64.84.213:8080/fhir/" 
 }
